1541c78fcd144023687fa32924d68947e39320a5
LucasZales <zaleslucas3@gmail.com>
2026-07-29 21:02:33 +1000

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""To kv module."""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple."""
+    return (k, float(v ** 2))
