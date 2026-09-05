e4eb639f616b12a7b52ea1d6a406c1ffd286d7dd
LucasZales <zaleslucas3@gmail.com>
2026-09-05 10:34:29 +1000

    task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+const createInt8TypedArray = (length, position, value) => {
+    if (position < 0 || position >= length) {
+        throw new Error('Position outside range');
+    }
+    const buffer = new ArrayBuffer(length);
+    const myArr = new DataView(buffer);
+    myArr.setInt8(position, value);
+    return myArr;
+};
+
+export default createInt8TypedArray;
