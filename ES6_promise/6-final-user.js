446577572b4460c5d4af67d3859e712ebdcec26b
LucasZales <zaleslucas3@gmail.com>
2026-09-10 11:10:46 +1000

    task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+const handleProfileSignup = (firstName, lastName, fileName) => {
+    const promise1 = signUpUser(firstName, lastName);
+    const promise2 = uploadPhoto(fileName);
+
+    return Promise.allSettled([promise1, promise2])
+        .then(results => results.map(result => {
+            if (result.status === 'fulfilled') {
+                return {
+                    status: result.status,
+                    value: result.value,
+                };
+            }
+
+            return {
+                status: result.status,
+                value: result.reason.toString(),
+            };
+        }));
+};
+
+export default handleProfileSignup;
