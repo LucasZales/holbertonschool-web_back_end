580cf7c233ea1fd27119f46a67b17050ebdfa0af
LucasZales <zaleslucas3@gmail.com>
2026-09-10 11:02:19 +1000

    task 3 fixed

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,12 +3,12 @@ import createUser from './utils';
 
 const handleProfileSignup = () => {
     return Promise.all([uploadPhoto(), createUser()])
-        .then(([photo, user]) => {
-            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        .then((values) => {
+            console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
         })
         .catch(() => {
             console.log('Signup system offline');
         });
-}
+};
 
 export default handleProfileSignup;
